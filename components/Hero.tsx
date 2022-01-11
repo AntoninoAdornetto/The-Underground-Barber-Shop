4ffc589793674cf24dea2bd0ab694ea4c5e7da88
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-01-11 14:33:18 -0500

    [ADD] Hero image component

---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '@/styles/components/Hero.module.scss';
+
+interface Props {
+  image: string;
+  rgba1?: string; // 32,33,36,.829
+  rgba2?: string; // 138,180,248,.274
+}
+
+const Hero: React.FC<Props> = ({ image, rgba1, rgba2 }) => {
+  if (rgba1 && rgba2) {
+    return (
+      <div
+        className={styles.heroDiv}
+        style={{
+          backgroundImage: `linear-gradient(to right bottom,rgba(${rgba1}),rgba(${rgba2})),url(${image})`,
+        }}
+      ></div>
+    );
+  }
+
+  return (
+    <div
+      className={styles.heroDiv}
+      style={{
+        backgroundImage: `url(${image})`,
+      }}
+    ></div>
+  );
+};
+
+export default Hero;
