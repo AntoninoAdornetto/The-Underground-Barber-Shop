5e39011a4b23a63025b3875de966101e8b461ae5
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-01-12 11:53:35 -0500

    [REMOVE]

--- tests/components/Hero.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Hero from '@components/Hero';
-
-describe('Hero Image component that loads an image onto the page', () => {
-  it('render an image to the page', () => {
-    render(<Hero image="hero.jpg" />);
-  });
-});
