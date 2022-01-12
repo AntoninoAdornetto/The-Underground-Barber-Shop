7b979d294cdc74b6ee8f8507d8a8ae5dd2fba5d0
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-01-12 18:07:53 -0500

    [FIX] spans not rendering

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import styles from '@styles/components/SectionTitle.module.scss';
-import Logo from './Logo';
 
 interface Props {
   text: string[];
   colors: string[];
+  fontSize: number;
   logo?: React.ReactNode;
 }
 
-const SectionTitle: React.FC<Props> = ({ text, colors, logo }) => {
+const SectionTitle: React.FC<Props> = ({ text, colors, logo, fontSize }) => {
   const renderText = text.map((word, i) => {
-    let index: number = i++;
+    let index: number = i + 1;
     if (index % 2 === 0) {
-      <span style={{ color: colors[0] }}>{word}</span>;
+      return (
+        <span key={word} style={{ color: colors[0], fontSize: `${fontSize}rem` }}>
+          {word}
+        </span>
+      );
     }
 
     if (index % 2 !== 0) {
-      <span style={{ color: colors[1] }}>{word}</span>;
+      return (
+        <span key={word} style={{ color: colors[1], fontSize: `${fontSize}rem` }}>
+          {word}
+        </span>
+      );
     }
   });
 
   return (
     <div className={styles.sectionTitleContainer}>
       {renderText}
-      {logo ? Logo : null}
+      {logo ? logo : null}
     </div>
   );
 };
