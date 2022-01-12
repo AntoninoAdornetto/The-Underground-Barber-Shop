3f0c48cb709a811860e8c7cbf83c9b88ea4bcb8b
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-01-12 16:12:54 -0500

    [ADD] About section jsx & styling

---
+++ components/About.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '@styles/components/About.module.scss';
+
+interface Props {
+  sectionTitle: React.ReactNode;
+  bio: string;
+  services: string[];
+}
+
+const About: React.FC<Props> = ({ sectionTitle, bio, services }) => {
+  // Section title
+  // Icon
+  // Bio Text
+  // List items of services
+  return (
+    <div className={styles.aboutSectionContainer}>
+      <div className={styles.titleContainer}>{sectionTitle}</div>
+      <div className={styles.bioContainer}>{bio}</div>
+      <div className={styles.servicesContainer}>{services}</div>
+    </div>
+  );
+};
+
+export default About;
