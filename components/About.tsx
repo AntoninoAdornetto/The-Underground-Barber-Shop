4c67480715b1005cb44206021448e73ee3faa7ae
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-01-13 16:24:06 -0500

    [EDIT] services list

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import styles from '@styles/components/About.module.scss';
 
 interface Props {
@@ -8,13 +10,23 @@ interface Props {
 }
 
 const About: React.FC<Props> = ({ sectionTitle, bio, services }) => {
+  const renderServices: React.ReactNode = services.map((service) => {
+    return (
+      <li>
+        <FontAwesomeIcon style={{ width: '1rem' }} icon={faCaretRight} />
+        <p>{service}</p>
+      </li>
+    );
+  });
   return (
     <section id="about" className={styles.aboutContainer}>
       <div className={styles.aboutTitle}>{sectionTitle}</div>
       <div className={styles.aboutBio}>
         <p>{bio}</p>
       </div>
-      <div className={styles.aboutServices}>{services}</div>
+      <div className={styles.aboutServices}>
+        <ul>{renderServices}</ul>
+      </div>
     </section>
   );
 };
