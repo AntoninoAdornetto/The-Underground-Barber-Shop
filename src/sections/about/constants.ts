078fe5d12d87d172d9ec9844517d9affade3462c
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-07-26 22:37:23 -0400

    [ADD] p

---
+++ src/sections/about/constants.ts
@@ -0,0 +1,17 @@
+export const ABOUT_TEXT = `
+						Biagio has cut and styled men’s hair since early 2001. Throughout this time
+						he has accrued a lot of experience that cannot easily be replicated. The
+						overall experience and haircut you receive is not to be compared with your
+						typical “great clips” barber who is fresh out of a local barber academy.
+						Most new age barbers know their way around clippers fairly well but when it
+						comes to additional details that require more precise work with scissors,
+						they simply cannot keep up with the amount of skill that Biagio maintains.
+						His countless amount of hours put him at the top of the chain as a true apex
+						barber.`;
+
+export const SERVICES = [
+	"Men's Hair Cut",
+	"Shampoo & Conditioner",
+	"Comedy Show",
+	"All for $30 plus any tip that goes towards his up and coming rap career",
+];
