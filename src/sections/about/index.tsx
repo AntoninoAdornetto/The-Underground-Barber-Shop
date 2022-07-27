dd8af1f5bfd668d82b3c5a2bf01de53d1893ebd3
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-07-27 00:39:50 -0400

    [EDIT] about page

--- src/sections/about/index.tsx
+++ src/sections/about/index.tsx
@@ -1,82 +1,43 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 import Section from "@/components/section";
 import styles from "./index.module.scss";
-import { ABOUT_TEXT } from "./constants";
+import { ABOUT_TEXT, SERVICES } from "./constants";
 
 const About: React.FC = () => {
-	const [serviceIndex, setServiceIndex] = useState<number>(0);
-	const [service, setService] = useState<string>(`Men's Hair Cut`);
+  const generateServices = () => {
+    return SERVICES.map((service) => {
+      return (
+        <li key={service}>
+          <FontAwesomeIcon icon={faCaretRight} />
+          {service}
+        </li>
+      );
+    });
+  };
 
-	const handleInterval = () => {
-		if (serviceIndex === services.length - 1) {
-			setService(0);
-		} else {
-			setService(serviceIndex++);
-		}
-	};
-
-	useEffect(() => {
-		const serviceInterval = setInterval(handleInterval, 1000);
-	});
-
-	{
-		/* const generateServices = () => { */
-	}
-	{
-		/* 	const services = [ */
-	}
-	{
-		/* 		"Men's Hair Cut", */
-	}
-	{
-		/* 		"Shampoo & Conditioner", */
-	}
-	{
-		/* 		"Comedy Show", */
-	}
-	{
-		/* 		"All of this for $30", */
-	}
-	{
-		/* 	]; */
-	}
-
-	{
-		/* 	return services.map((service) => { */
-	}
-	{
-		/* 		return <li key={service}>{`> ${service} `}</li>; */
-	}
-	{
-		/* 	}); */
-	}
-	{
-		/* }; */
-	}
-
-	return (
-		<React.Fragment>
-			<Section sectionId="about" className={styles.aboutContainer}>
-				<div>
-					<h2 className="secondary">
-						biagio's <span className="dark">vision</span>
-					</h2>
-					<Image src="/icons/eye--icon.png" width="75" height="75" />
-				</div>
-				<article>
-					<p>{ABOUT_TEXT}</p>
-				</article>
-				<div>
-					<h3 className="dark">Services & price:</h3>
-				</div>
-				<ul>
-					<li>{service}</li>
-				</ul>
-			</Section>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Section sectionId="about" className={styles.aboutContainer}>
+        <div>
+          <h2 className="secondary">
+            biagio's <span className="dark">vision</span>
+          </h2>
+          <Image src="/icons/eye--icon.png" width="75" height="75" />
+        </div>
+        <article>
+          <p>{ABOUT_TEXT}</p>
+        </article>
+        <div>
+          <h3 className="dark">Services & price:</h3>
+        </div>
+        <ul>{generateServices()}</ul>
+      </Section>
+    </React.Fragment>
+  );
 };
 
 export default About;
